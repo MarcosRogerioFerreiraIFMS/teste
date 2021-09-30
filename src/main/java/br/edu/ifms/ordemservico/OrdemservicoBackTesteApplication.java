a2e76107ca2e44985cf4a786db2712a8afce4878
MarcosRogerioFerreiraIFMS <marcos.rogerio@ifms.edu.br>
2021-09-30 15:49:22 -0400

    first commit

---
+++ src/main/java/br/edu/ifms/ordemservico/OrdemservicoBackTesteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.ordemservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemservicoBackTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemservicoBackTesteApplication.class, args);
+	}
+
+}
